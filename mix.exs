9d5147285a679bccc56965f8a4cde0602d83ee32
jbb01 <git@jonahbauer.eu>
2023-09-01 17:56:31 +0200

    update version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ChatBot.MixProject do
   def project do
     [
       app: :chat_bot,
-      version: "0.1.6",
+      version: "0.1.7",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps()
